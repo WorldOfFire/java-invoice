c97a0af1efd9985ef7032eaf86ec21fba210466e
Weronika Szulinska <szulinska@student.agh.edu.pl>
2025-02-08 13:55:24 +0100

    ProductTest green

--- src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
@@ -5,5 +5,11 @@ import java.math.BigDecimal;
 public class TaxFreeProduct extends Product {
     public TaxFreeProduct(String name, BigDecimal price) {
         super(name, price, BigDecimal.ZERO);
+        if(name.isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if(price.intValue() < 2){
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
     }
 }
